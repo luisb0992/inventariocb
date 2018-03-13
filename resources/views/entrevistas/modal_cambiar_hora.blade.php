c3a013699d2b2e2b4fbfc8b2c283fe3403992276
luisb0992 <luisbarrios0992@gmail.com>
2018-03-12 21:37:41 -0430

    edit de entrevistas en proceso

---
+++ resources/views/entrevistas/modal_cambiar_hora.blade.php
@@ -0,0 +1,31 @@
+<div class="modal fade" tabindex="-1" role="dialog" id="cambiar_hora">
+	<div class="modal-dialog modal-sm" role="document">
+		<div class="modal-content">
+				<div class="">
+					<buttton class="close" type="button" data-dismiss="modal">&times;</buttton>
+					<div class="label-primary text-center padding_1em">
+						<h3><i class="fa fa-edit"></i> Actualizar Hora</h3>
+					</div>
+					<div class="modal-body">
+						<div class="row">
+							<div class="col-sm-12">
+								<input type="hidden" name="_token" id="token" value="{{ csrf_token() }}">
+								<input type="text" id="hora" class="form-control timepicker">
+							</div>
+						</div>
+					</div>
+				</div>
+				<div class="padding_1em">
+					<div class="text-right">
+						<input type="button" class="btn" data-dismiss="modal" value="Cerrar">
+						<button type="button" class="btn btn-primary btn_save_comentario">
+							<i class="fa fa-save"></i> Guardar
+						</button>
+						<span id="reload_coment" style="display: none">
+							<i class="fa fa-refresh fa-pulse fa-fw"></i>
+						</span>
+					</div>
+				</div>
+		</div>	
+	</div>
+</div>
