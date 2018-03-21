2442d11ee5973b6d12b71b3c29970f0f1cbe9d61
luisb0992 <luisbarrios0992@gmail.com>
2018-03-20 20:59:10 -0430

    links añadidos a imagenes de dashboard

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -11,18 +11,27 @@
 	<div class="row">
 		<div class="col-sm-12">
 			<div class="padding_1em bg-primary">
-				<h1>Bienvenido {{ \Auth::user()->name }}</h1>
+				<h4>Bienvenido {{ \Auth::user()->name }}</h4>
 			</div>
-		<hr>
+			<br>
 		</div>
-		<div class="col-sm-4 border_right_1">   
-			<img src="{{ asset('img/dashboard_1.png') }}" alt="dashboard" class="img-responsive text-center col-sm-10">
+		<div class="col-sm-4 border_right_1">
+			<a href="{{ url('articulos') }}">   
+				<img src="{{ asset('img/dashboard_3.png') }}" alt="articulos" class="img_hover img-responsive text-center col-sm-10">
+			</a>
+			<span class="h3"><i>Articulos</i></span>
 		</div>
-		<div class="col-sm-4 border_right_1">   
-			<img src="{{ asset('img/dashboard_2.png') }}" alt="dashboard" class="img-responsive text-center col-sm-10">
+		<div class="col-sm-4 border_right_1">
+			<a href="{{ url('entrevistas') }}">   
+				<img src="{{ asset('img/dashboard_2.png') }}" alt="entrevistas" class="img_hover img-responsive text-center col-sm-10">
+			</a>
+			<span class="h3"><i>Entrevistas</i></span>
 		</div>
-		<div class="col-sm-4">   
-			<img src="{{ asset('img/dashboard_3.png') }}" alt="dashboard" class="img-responsive text-center col-sm-10">
+		<div class="col-sm-4">
+			<a href="{{ url('ventas') }}">   
+				<img src="{{ asset('img/dashboard_1.png') }}" alt="ventas" class="img_hover img-responsive text-center col-sm-10">
+			</a>
+			<span class="h3"><i>Ventas</i></span>
 		</div>		
   	</div>
 @endsection
